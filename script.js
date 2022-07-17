10ed111a4995ffeebed35afc7b68d2ec3873739d
It's BOBIK <alex.chpak@gmail.com>
2022-07-17 10:34:10 +0200

    first develop of feature 6, change text features

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 // блок в 100% высоты feature-2
 setInterval(setSize, 10);
+setInterval(setSize__collateral, 10);
+setInterval(setHeight__collateral, 10);
 
 function setSize() {
 	var heightFeatureTextDecoration = document.getElementById('firstElement').clientHeight;
 	heightFeatureTextDecoration = String(heightFeatureTextDecoration+"px");
 	var secondElement = document.getElementById('secondElement');
 	secondElement.style.height = heightFeatureTextDecoration;
+}
+
+function setSize__collateral() {
+	var widthFeatureTextDecoration = document.getElementById('collateralText').clientWidth;
+	widthFeatureTextDecoration = String(widthFeatureTextDecoration+"px");
+	var secondElement = document.getElementById('collateralElement');
+	secondElement.style.width = widthFeatureTextDecoration;
+}
+
+function setHeight__collateral() {
+	var firstElementHeight = document.getElementById('collateralText').clientHeight;
+	firstElementHeight = String((firstElementHeight+7)+"px");
+	var secondElement = document.getElementById('collateralElement');
+	secondElement.style.top = firstElementHeight;
 }
 
 
