43bb36f96b2a8da6eecb7fc364f999328cd43229
It's BOBIK <alex.chpak@gmail.com>
2022-07-29 10:43:02 +0200

    fix php server form

--- contact.php
+++ contact.php
@@ -4,8 +4,8 @@ if($_POST) {
 	$name = "";
 	$email = "";
 	$phone = "";
-	$message = "";
-	$recipient = "alex.chpak@gmail.com";
+	$visitor_message = "";
+	$recipient = "linguirus@yandex.ru";
 	 
 	if(isset($_POST['name'])) {
 		$name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
@@ -20,19 +20,32 @@ if($_POST) {
 			$phone = filter_var($_POST['phone'], FILTER_SANITIZE_STRING);
 	}
 	 
-	if(isset($_POST['message'])) {
-			$visitor_message = htmlspecialchars($_POST['message']);
-			// не стал проверять через filter var так как в этом нет необходимости
+	if(isset($_POST['visitor_message'])) {
+			$visitor_message = htmlspecialchars($_POST['visitor_message']);
 	}
 
+    $subject = "НОВАЯ ЗАЯВКА от $email";
+
 	$headers  = 'MIME-Version: 1.0' . "\r\n"
-	.'Content-type: text/html; charset=utf-8' . "\r\n"
-	.'From: ' . $email . "\r\n"
-	.'Phone: ' . $phone . "\r\n"
-	."Name: " . $name . "\r\n";
+	.'Content-type: text/html; charset=utf-8' . "\r\n";
+
+	$message = "
+		<html>
+			<head>
+				<title>ЗАЯВКА</title>
+			</head>
+			<body>
+				<p>🔔 Новая заявка! 🔔</p>
+				<p>Имя: $name</p>
+				<p>Mail: $email</p>
+				<p>Телефон: $phone</p>
+				<p>Сообщениe: <br> $visitor_message</p>
+			</body>
+		</html>
+	";
 	 
-	if(mail($recipient, $message, $headers)) {
-			echo "<p>Спасибо, что отсавили заявку, $name. Скоро мы с Вами свяжемся!</p>;
+	if(mail($recipient, $subject, $message, $headers)) {
+			echo "<p>Спасибо, что отсавили заявку, $name. Скоро мы с Вами свяжемся!</p>";
 	} else {
 			echo '<p>Извините, но мы не можем отправить Вашу форму :( <br> Переотправьте форму.</p>';
 	}
